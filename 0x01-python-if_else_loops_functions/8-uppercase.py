9e7a120ceee5da23120e60a52f3888fec8881f27
mz-jennytech <okosisijenifer@gmail.com>
2022-11-29 20:27:50 -0800

    updated task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) > 96:
-        return True
+def to_uper(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
     else:
-        return False
+        return ord(character)
+
+
+def uppercase(string):
+    string_new = ""
+    for character in string:
+        string_new += "%c" % to_uper(character)
+    print("{:s}".format(string_new))
