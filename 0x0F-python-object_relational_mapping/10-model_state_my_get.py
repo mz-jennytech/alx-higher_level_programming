370e1db2a9a6ac31eed349f82ed626ac0707c394
mz-jennytech <okosisijenifer@gmail.com>
2023-03-21 07:40:14 -0700

    MySql db

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-"""print first object"""
+"""
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
 
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-import sys
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2],
-                                                    sys.argv[3]),
-        pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    """
+    Access to the database and get a state
+    from the database.
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    count = 0
-    for row in session.query(State).filter(
-            State.name == sys.argv[4]).order_by(State.id):
-            print(row.id)
-            count = count + 1
-    if count == 0:
+
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state is not None:
+        print('{0}'.format(state.id))
+    else:
         print("Not found")
-    session.close()
