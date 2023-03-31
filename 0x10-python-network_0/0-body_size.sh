7273844569fe7a6b6479ebc079f501d95ec5359c
mz-jennytech <okosisijenifer@gmail.com>
2023-03-30 22:29:45 -0700

    py

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes a uRL, sends a request and displays the size of the body
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# send a request to an URL with curl and displays the size of the body of the response
+curl -s -w '%{size_download}' -o /dev/null "$1"
