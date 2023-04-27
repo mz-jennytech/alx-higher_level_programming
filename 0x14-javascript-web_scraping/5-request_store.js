6700b1d117f09b1876552c674bc1d9fbb1caebe3
mz-jennytech <okosisijenifer@gmail.com>
2023-04-26 19:57:53 -0700

    js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
-// copy content of a web in a file
-const myRequest = require('request');
-const myUrl = process.argv[2];
+
+// a script to writr to a file
+
+const request = require('request');
 const fs = require('fs');
-myRequest(myUrl, function (err, res, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(process.argv[3], body, 'utf8', function (err, data) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
+const filePath = process.argv[3];
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  fs.writeFile(filePath, body, err => {
+    if (err) console.error(err);
+  });
 });
